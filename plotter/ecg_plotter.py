b8995377ca8924a1f097e2a56720525d28b69655
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-05-24 01:52:19 -0300

    feat: agrego ploteo de irfft

--- plotter/ecg_plotter.py
+++ plotter/ecg_plotter.py
@@ -1,4 +1,3 @@
-import numpy as np
 import dearpygui.dearpygui as dpg
 import serial.tools.list_ports
 import serial
@@ -23,6 +22,9 @@ class ECGPlotter():
         self._freqs = [0.0]
         self._fft_real= [0.0]
         self._fft_filtered = [0.0]
+        self._time = [0.0]
+        self._ifft_real = [0.0]
+        self._ifft_filtered = [0.0]
 
         # Crear la ventana de selección de puerto serial
         with dpg.window(label="App", tag="app_window"):
@@ -41,30 +43,37 @@ class ECGPlotter():
                 dpg.add_text("", tag="serial_status")
 
             # Configuro una ventana para el ploteo de la FFT
-            with dpg.child_window(tag="fft_window", width=self._width, height=(2 * self._heigth // 5)):
+            with dpg.child_window(tag="fft_window", width=self._width, height=(3 * self._heigth // 7)):
                 with dpg.plot(label="FFT Plot", height=-1, width=-1, tag="fft_plot"):
-                    x_axis = dpg.add_plot_axis(dpg.mvXAxis, label="Frequency [Hz]", tag="x_axis")
-                    y_axis = dpg.add_plot_axis(dpg.mvYAxis, label="Magnitude [V]", tag="y_axis")
+                    x_axis = dpg.add_plot_axis(dpg.mvXAxis, label="Frequency [Hz]", tag="freq_axis")
+                    y_axis = dpg.add_plot_axis(dpg.mvYAxis, label="Magnitude [V]", tag="fft_axis")
                     
                     dpg.add_line_series([], [], label="FFT (real)", parent=y_axis, tag="fft_real")
                     dpg.add_line_series([], [], label="FFT (filtrada)", parent=y_axis, tag="fft_filtered")
 
                     # Fijar los límites de los ejes
-                    dpg.set_axis_limits("x_axis", 0, 250)
-                    dpg.set_axis_limits("y_axis", 0, 2)
+                    dpg.set_axis_limits("freq_axis", 0, 250)
+                    dpg.set_axis_limits("fft_axis", 0, 2)
 
                     # Muestro la etiqueta
                     dpg.add_plot_legend(parent="fft_plot")
             
 
             # Configuro una ventana para el ploteo de la FFT
-            with dpg.child_window(tag="ifft_window", width=self._width, height=(2 * self._heigth // 5)):
-                with dpg.plot(label="IFFT Plot", height=-1, width=-1):
-                    dpg.add_plot_axis(dpg.mvXAxis, label="Time [s]")
-                    y_axis = dpg.add_plot_axis(dpg.mvYAxis, label="Magnitude [V]")
+            with dpg.child_window(tag="ifft_window", width=self._width, height=(3 * self._heigth // 7)):
+                with dpg.plot(label="IFFT Plot", height=-1, width=-1, tag="ifft_plot"):
+                    x_axis = dpg.add_plot_axis(dpg.mvXAxis, label="Time [s]", tag="time_axis")
+                    y_axis = dpg.add_plot_axis(dpg.mvYAxis, label="Magnitude [V]", tag="ifft_axis")
                     
                     dpg.add_line_series([], [], label="IFFT (real)", parent=y_axis, tag="ifft_real")
                     dpg.add_line_series([], [], label="IFFT (filtrada)", parent=y_axis, tag="ifft_filtered")
+
+                    # Fijar los límites de los ejes
+                    dpg.set_axis_limits("time_axis", 0, 2.048)
+                    dpg.set_axis_limits("ifft_axis", 0, 4)
+
+                    # Muestro la etiqueta
+                    dpg.add_plot_legend(parent="ifft_plot")
 
         # Conectar el callback de redimensionamiento
         dpg.set_viewport_resize_callback(self._resize_window_callback)
@@ -84,9 +93,13 @@ class ECGPlotter():
                     # Leo y decodifico el JSON
                     data = self._port.readline().decode().strip()
                     data = json.loads(data)
+                    # Veo si hay datos
                     self._freqs = data.get("freqs", self._freqs)
                     self._fft_real = data.get("fft_real", self._fft_real)
                     self._fft_filtered = data.get("fft_filtered", self._fft_filtered)
+                    self._time = data.get("time", self._time)
+                    self._ifft_real = data.get("ifft_real", self._ifft_real)
+                    self._ifft_filtered = data.get("ifft_filtered", self._ifft_filtered)
 
             self._update_plot()
             self._refresh_ports()
@@ -101,18 +114,11 @@ class ECGPlotter():
         """
         Actualiza la informacion del ploteo
         """
-        import random
-
-        # Obtengo los puntos de la IFFT
-        t = np.linspace(0, 100e-3, 1024, False)  # 100 miliseconds
-        signal = np.sin(2 * np.pi * 100 * random.random() * t)  # señal aleatoria
-        dpg.set_value('ifft_real', [t, signal])
-        signal = np.sin(2 * np.pi * 100 * random.random() * t)  # señal aleatoria
-        dpg.set_value('ifft_filtered', [t, signal])
-
         # Actualizar los valores del gráfico
         dpg.set_value("fft_real", [self._freqs, self._fft_real])
         dpg.set_value("fft_filtered", [self._freqs, self._fft_filtered])
+        dpg.set_value("ifft_real", [self._time, self._ifft_real])
+        dpg.set_value("ifft_filtered", [self._time, self._ifft_filtered])
 
     
     def _resize_window_callback(self, sender, app_data):
@@ -122,11 +128,11 @@ class ECGPlotter():
         # Obtengo las dimensiones del viewport y las configuro para la ventana
         width, height = dpg.get_viewport_client_width(), dpg.get_viewport_client_height()
         dpg.set_item_width("serial_window", width)
-        dpg.set_item_height("serial_window", height // 5)
-        dpg.set_item_width("ifft_window", width)
-        dpg.set_item_height("ifft_window", 2 * height // 5)
+        dpg.set_item_height("serial_window", height // 7)
         dpg.set_item_width("fft_window", width)
-        dpg.set_item_height("fft_window", 2 * height // 5)
+        dpg.set_item_height("fft_window", 3 * height // 7)
+        dpg.set_item_width("ifft_window", width)
+        dpg.set_item_height("ifft_window", 3 * height // 7)
 
     
     def _refresh_ports(self):
@@ -151,28 +157,3 @@ class ECGPlotter():
                 dpg.set_value(item="serial_status", value=f"Puerto {app_data} conectado con exito!")
             except:
                 dpg.set_value(item="serial_status", value="Error conectando al puerto!")
-
-    
-    def _generate_fft_data(self, samples: int = 1024, sample_rate: int = 1000) -> tuple:
-        """
-        Genera muestra aleatoria de datos para la FFT
-
-        Args: 
-            samples (int): Cantidad de muestras
-            sample_rate (float): Frecuencia de muestreo
-
-        Return:
-            Tupla con el array de frecuencias y magnitudes
-        """
-
-        t = np.linspace(0, 1, samples, False)  # 1 second
-        signal = np.random.normal(0, 1, samples)  # señal aleatoria
-        fft_result = np.fft.fft(signal)
-        freqs = np.fft.fftfreq(samples, 1/sample_rate)
-        
-        # Filtrar solo las frecuencias positivas y menores a 100 Hz
-        idx = np.where((freqs >= 0) & (freqs <= 100))
-        freqs = freqs[idx]
-        fft_magnitude = np.abs(fft_result[idx])
-        
-        return freqs, fft_magnitude
