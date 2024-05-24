565b9c449b5cbf2e1ac214a311a053af6b7c7e77
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-05-23 21:07:09 -0300

    feat: agrego vista para la ifft

--- plotter/ecg_plotter.py
+++ plotter/ecg_plotter.py
@@ -20,7 +20,7 @@ class ECGPlotter():
         # Crear la ventana de selección de puerto serial
         with dpg.window(label="App", tag="app_window"):
 
-            with dpg.child_window(tag="serial_window", width=self._width, height=(self._heigth // 4)):
+            with dpg.child_window(tag="serial_window", width=self._width, height=(self._heigth // 5)):
                 dpg.add_text("Seleccione un puerto serial:")
                 
                 # Listar los puertos seriales disponibles y crear el combo box
@@ -32,13 +32,23 @@ class ECGPlotter():
                     dpg.add_text("No se encontraron puertos seriales.")
 
             # Configuro una ventana para el ploteo de la FFT
-            with dpg.child_window(tag="fft_window", width=self._width, height=(2 * self._heigth // 4)):
+            with dpg.child_window(tag="fft_window", width=self._width, height=(2 * self._heigth // 5)):
                 with dpg.plot(label="FFT Plot", height=-1, width=-1):
                     dpg.add_plot_axis(dpg.mvXAxis, label="Frequency [Hz]")
                     y_axis = dpg.add_plot_axis(dpg.mvYAxis, label="Magnitude [V]")
                     
                     dpg.add_line_series([], [], label="FFT (real)", parent=y_axis, tag="fft_real")
                     dpg.add_line_series([], [], label="FFT (filtrada)", parent=y_axis, tag="fft_filtered")
+            
+
+            # Configuro una ventana para el ploteo de la FFT
+            with dpg.child_window(tag="ifft_window", width=self._width, height=(2 * self._heigth // 5)):
+                with dpg.plot(label="IFFT Plot", height=-1, width=-1):
+                    dpg.add_plot_axis(dpg.mvXAxis, label="Time [s]")
+                    y_axis = dpg.add_plot_axis(dpg.mvYAxis, label="Magnitude [V]")
+                    
+                    dpg.add_line_series([], [], label="IFFT (real)", parent=y_axis, tag="ifft_real")
+                    dpg.add_line_series([], [], label="IFFT (filtrada)", parent=y_axis, tag="ifft_filtered")
 
         # Conectar el callback de redimensionamiento
         dpg.set_viewport_resize_callback(self._resize_window_callback)
@@ -64,6 +74,15 @@ class ECGPlotter():
         """
         Actualiza la informacion del ploteo
         """
+        import random
+
+        # Obtengo los puntos de la IFFT
+        t = np.linspace(0, 100e-3, 1024, False)  # 100 miliseconds
+        signal = np.sin(2 * np.pi * 100 * random.random() * t)  # señal aleatoria
+        dpg.set_value('ifft_real', [t, signal])
+        signal = np.sin(2 * np.pi * 100 * random.random() * t)  # señal aleatoria
+        dpg.set_value('ifft_filtered', [t, signal])
+
         # Obtengo los puntos de la FFT
         freqs, fft_magnitude = self._generate_fft_data()
         # Actualizar los valores del gráfico
@@ -81,9 +100,11 @@ class ECGPlotter():
         # Obtengo las dimensiones del viewport y las configuro para la ventana
         width, height = dpg.get_viewport_client_width(), dpg.get_viewport_client_height()
         dpg.set_item_width("serial_window", width)
-        dpg.set_item_height("serial_window", height // 4)
+        dpg.set_item_height("serial_window", height // 5)
+        dpg.set_item_width("ifft_window", width)
+        dpg.set_item_height("ifft_window", 2 * height // 5)
         dpg.set_item_width("fft_window", width)
-        dpg.set_item_height("fft_window", 3 * height // 4)
+        dpg.set_item_height("fft_window", 2 * height // 5)
 
     
     def _port_selected_callback(self, sender, app_data):
